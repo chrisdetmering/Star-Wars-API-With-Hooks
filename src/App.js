da6951d4d178a57341cf6bd390d663b676408297
Joe Mittiga <joemittiga7@hotmail.com>
2020-11-05 16:42:56 -0500

    updated code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import axios from "axios"
+import Table from "./components/Table"
 
-function App() {
-  return (
-    <div className="App">
-     
+const App = () => {
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [planets, setPlanets] = useState([])
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const result = await axios('https://swapi.dev/api/people/?page=1')
+      const planetResult = await axios('https://swapi.dev/api/planets/?page=1')
+
+      console.log(result.data.results)
+      setCharacters(result.data.results)
+      setPlanets(planetResult.data.results)
+      console.log(planetResult.data.results)
+    }
+
+    fetchCharacters();
+  }, [])
+  
+  
+  return(
+    <div>
+      
+      <Table characters={characters} planets={planets}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
