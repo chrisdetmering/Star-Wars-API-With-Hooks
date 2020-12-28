0185e4feb907f79cde18ab5275ce860d13da0e27
Joe Mittiga <joemittiga7@hotmail.com>
2020-12-27 21:54:48 -0500

    updated code

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,26 +6,22 @@ const Input = (props) => {
 
     const handleChange = (e) => {
         setText(e.target.value)
+}; 
 
-        if (text.length > 0) {
-            props.characters.filter((character) => {
-                return character.name.match(text);
-            })
-        }
-    } 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    props.characters(text);
+};
 
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <input 
-            type="text" 
-            placeholder="Search Characters"
-            value={text}
-            onChange={handleChange}
+                type="text"
+                placeholder="Search Characters"
+                onChange={handleChange}
+                value={text}
             />
-
-           
-            
-        </div>
+        </form>
     )
 }
 
