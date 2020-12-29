b6d7c52f69a1c6caa7d23a51da931458db06e1e8
Joe Mittiga <joemittiga7@hotmail.com>
2020-12-29 14:37:18 -0500

    updated code

--- src/App.js
+++ src/App.js
@@ -9,58 +9,55 @@ import Input from './components/Input'
 const App = () => {
   const [characters, setCharacters] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-  
+
   // Fetch API data
 
   useEffect(() => {
-      const fetchCharacters = async () => {
+    const fetchCharacters = async () => {
       const response = await axios(`https://swapi.dev/api/people/?page=1`)
       const speciesResponse = await axios('https://swapi.dev/api/species/?page=1')
       console.log(response.data.results);
-      
+
       for (const character of response.data.results) {
         const homeworld = await axios.get(character.homeworld);
         character.homeworld = homeworld.data.name;
 
-        const species = await axios.get(character.species); 
+        const species = await axios.get(character.species);
         !species.data.name ? character.species = "Human" : character.species = species.data.name;
       }
       setCharacters(response.data.results)
       setIsLoading(false);
     }
-    
     fetchCharacters();
-  
   }, [])
 
 
   // Search Function
-  
-  const characterSearch = async (searchTerm) => {
-      setIsLoading(true);
-      const characterSearchResponse = await axios.get(
-        `https://swapi.dev/api/people/?search=${searchTerm}`
-      )
 
-      for (const character of characterSearchResponse.data.results) {
-        const homeworld = await axios.get(character.homeworld);
-        character.homeworld = homeworld.data.name;
-      
+  const characterSearch = async (searchTerm) => {
+    setIsLoading(true);
+    const characterSearchResponse = await axios.get(
+      `https://swapi.dev/api/people/?search=${searchTerm}`
+    )
+
+    for (const character of characterSearchResponse.data.results) {
+      const homeworld = await axios.get(character.homeworld);
+      character.homeworld = homeworld.data.name;
 
       const species = await axios.get(character.species);
       !species.data.name
-      ? (character.species = "Human")
-      : (character.species = species.data.name);
-  }
-  setCharacters(characterSearchResponse.data.results);
-  setIsLoading(false);
+        ? (character.species = "Human")
+        : (character.species = species.data.name);
+    }
+    setCharacters(characterSearchResponse.data.results);
+    setIsLoading(false);
   };
-  
-  return(
-    <div>
-        <h1 className="header">Star Wars API</h1>
 
-      <Input characters={characterSearch} isLoading={isLoading}/> 
+  return (
+    <div>
+      <h1 className="header">Star Wars API</h1>
+
+      <Input characters={characterSearch} isLoading={isLoading} />
       <Table characters={characters} isLoading={isLoading} />
     </div>
   )
