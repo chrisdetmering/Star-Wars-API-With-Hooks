75a4d0a59dc9d24f97a44acf1af8afc70939413e
Joe Mittiga <joemittiga7@hotmail.com>
2020-12-30 17:13:13 -0500

    updated code

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,27 +1,14 @@
 import React, { useState } from 'react'
 
 const Input = (props) => {
-    const [text, setText] = useState('')
-
-    const handleChange = (e) => {
-        setText(e.target.value)
-        console.log(text);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.characters(text);
-    };
-
+    
     return (
-        <form className="input" onSubmit={handleSubmit}>
+        <form className="input">
             <input
                 type="text"
                 placeholder="Search Characters"
-                onChange={handleChange}
-                value={text}
+                onChange={(e) => props.characters(e.target.value)}
             />
-            <button>Search</button>
         </form>
     )
 }
