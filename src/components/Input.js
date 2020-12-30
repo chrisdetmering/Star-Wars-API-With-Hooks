d1aeb27740f48b1cffa3f0f2b880c7a3110b6050
Joe Mittiga <joemittiga7@hotmail.com>
2020-12-30 17:42:48 -0500

    updated code

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,13 +3,13 @@ import React, { useState } from 'react'
 const Input = (props) => {
     
     return (
-        <form className="input">
+        <div className="input">
             <input 
                 type="text"
                 placeholder="Search Characters"
                 onChange={(e) => props.characters(e.target.value)}
             />
-        </form>
+        </div>
     )
 }
 
